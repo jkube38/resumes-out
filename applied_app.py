c3d2c7b78bde25a0883a00823a19f66e01e5c833
Jordan Kubista <jkube29@gmail.com>
2021-09-17 11:09:23 -0500

    initial commit

---
+++ applied_app.py
@@ -0,0 +1,8 @@
+'''The applied apps keep track of the companies and positions I've applied to
+   for a quick search to make sure it was not already applied to'''
+
+__author__ = "Jordan Kubista"
+
+import sys
+import argparse
+from applied import applied_to
